6896ab1478f4379d3f41d7dbee47e7fc384238ad
Demaris Ashmore <demiashmore@yahoo.com>
2022-07-30 22:56:23 -0600

    commit tree dc328b3c56633754db9a6ebda0ba114d5e82d4b3

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    // let par = document.querySelector('p')
+    let par = document.querySelector('#text')
+    par.textContent = "This is really cool!"
+    // console.log('Dom is loaded')
+})
+
